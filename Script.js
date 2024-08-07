6535bfa5a44605c6933c744413aeafda7cf9dd33
mohammadkanaan5 <106881901+mohammadkanaan5@users.noreply.github.com>
2024-08-07 20:38:15 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,13 @@
+// responsive
+
+function respNav() {
+
+    var x = document.getElementById("nav");
+    if (x.className === "navbar") {
+
+      x.className += " responsive";
+    } else {
+      
+      x.className = "navbar";
+    }
+  }
